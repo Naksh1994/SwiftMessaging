3b977ae378c4a51766771c1eac508f88f3330446
Naksh1994 <99979363+Naksh1994@users.noreply.github.com>
2025-03-19 20:49:49 +0530

    Initial commit - SwiftUI Messaging App

---
+++ SwiftMessaging/SwiftMessagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMessagingApp.swift
+//  SwiftMessaging
+//
+//  Created by Nakshatra Verma on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMessagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
